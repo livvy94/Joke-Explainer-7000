5807ba6eb69919b62144b9a241bec8a1ca16e16b
ddavader <bechichi99@gmail.com>
2024-08-28 03:11:57 +0800

    QoC code and unit testing

---
+++ simpleQoC.py
@@ -0,0 +1,356 @@
+import os
+from pathlib import Path
+from inspect import getsourcefile
+import requests
+import cgi
+
+from mutagen import File, FileType, flac, wave
+from scipy.io import wavfile
+import subprocess
+import numpy as np
+
+DOWNLOAD_DIR = Path(os.path.abspath(getsourcefile(lambda:0))).parent / 'audioDownloads'
+
+#=======================================#
+#               DEBUGGING               #
+#=======================================#
+DEBUG_MODE = False
+
+def DEBUG(msg):
+    if DEBUG_MODE:
+        print(msg)
+
+#=======================================#
+#          EXCEPTION HANDLING           #
+#=======================================#
+
+# https://stackoverflow.com/a/26938914
+class QoCException(Exception):
+    def __init__(self, message, *args):
+        self.message = message # without this you may get DeprecationWarning
+  
+        # allow users initialize misc. arguments as any other builtin Error
+        super(QoCException, self).__init__(message, *args) 
+
+
+#=======================================#
+#           URL DOWNLOADING             #
+#=======================================#
+
+def parseUrl(url: str) -> str:
+    """
+    Certain domains have to be treated in a unique way in order to download files
+    For now this function just "converts" the given URL to the "downloadable" version,
+    depending on the detected domain
+    """
+    if url.find('siiva-gunner.com/?id=') != -1:
+        return url.replace('?id=', 'api/v2/file/')
+    
+    if url.find('185.142.239.147/?id=') != -1:
+        # probably don't wanna deal with SSL certificate stuff
+        return url.replace('185.142.239.147/?id=', 'siiva-gunner.com/api/v2/file/')
+    
+    if url.find('drive.google.com') != -1:
+        """
+        Assumes the following, taken from moder's scheduler program:
+            // Handles 3 kinds of links (they can be preceeded by https://):
+            // - drive.google.com/open?id=FILEID
+            // - drive.google.com/file/d/FILEID/view?usp=sharing
+            // - drive.google.com/uc?id=FILEID&export=download
+        """
+        id = ""
+        if url.find("open?id=") != -1:
+            id = url.split("open?id=")[1].split("&")[0]
+        if url.find("file/d/") != -1:
+            id = url.split("file/d/")[1].split("/")[0]
+        if url.find("uc?id=") != -1:
+            id = url.split("uc?id=")[1].split("&")[0]
+
+        if id == "":
+            raise QoCException("Drive ID cannot be detected from URL: {}".format(url))
+        else:
+            # let's hope google doesn't randomly decide to change how downloading works in the future...
+            return "https://drive.usercontent.google.com/download?id={}&export=download&confirm=t".format(id)
+    
+    if url.find('dropbox.com') != -1:
+        return url.replace('&dl=0', '&dl=1')
+
+    return url
+
+# https://stackoverflow.com/questions/38511444/python-download-files-from-google-drive-using-url
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
+
+
+def downloadAudioFromUrl(url: str) -> str:
+    url = parseUrl(url)
+    filepath = None
+
+    try:
+        session = requests.Session()
+        response = session.get(url, stream=True)
+
+        _, params = cgi.parse_header(response.headers['Content-Disposition'])
+        filename = params['filename']
+        filepath = DOWNLOAD_DIR / filename
+
+        save_response_content(response, filepath)
+
+    except KeyError:
+        # If header does not contain Content-Disposition
+        raise QoCException('Filename cannot be parsed from the URL (it may be an invalid link).')
+    
+    # https://stackoverflow.com/questions/16511337/correct-way-to-try-except-using-python-requests-module
+    except requests.exceptions.Timeout:
+        raise QoCException('Request timed out.')
+    except requests.exceptions.TooManyRedirects:
+        raise QoCException('Bad URL.')
+    except requests.exceptions.RequestException as e: # Other errors
+        raise QoCException('Unknown URL error. {}'.format(e.strerror))
+
+    DEBUG('Downloaded filepath: {}'.format(filepath))
+    return filepath
+
+
+def parseAudio(filepath: str) -> FileType:
+    return File(filepath)
+
+
+#=======================================#
+#           BITRATE CHECKING            #
+#=======================================#
+
+def checkBitrate(file: FileType) -> str:
+    """
+    Check the bitrate of a mutagen File.
+    Requires either lossless format or the metadata contains bitrate information.
+    If not, raises QoCException with the file metadata.
+    """
+    if isinstance(file, wave.WAVE) or isinstance(file, flac.FLAC):
+        return ":check: Lossless file is OK."
+    
+    # seems video files show lower bitrate on properties view for some reason, shouldn't be an issue generally
+    if hasattr(file.info, 'bitrate'):
+        bitrate = file.info.bitrate
+        if bitrate < 320000:
+            return ":fix: The {} file's bitrate is {}kbps. Please re-render at 320kbps.".format(type(file).__name__, bitrate // 1000)
+        else:
+            return ":check: Bitrate is OK."
+    
+    raise QoCException("ERROR: Unknown bitrate. File metadata: {}".format(file.pprint()))
+
+
+#=======================================#
+#           CLIPPING CHECKING           #
+#=======================================#
+
+def convertToWAV(filepath: str, wav_filepath: str):
+    """
+    Runs ffmpeg to create a WAV file from the provided audio filepath
+    """
+    try:
+        subprocess.call([
+            'ffmpeg',
+            '-hide_banner',
+            '-loglevel', 'error',
+            '-i', Path(filepath),
+            '-c:a', 'pcm_f32le',
+            wav_filepath,
+        ], shell=True)
+    except FileNotFoundError:
+        raise QoCException("ERROR: ffmpeg failed to run (make sure the command 'ffmpeg' can run).")
+    
+    if not os.path.exists(wav_filepath):
+        raise QoCException("ERROR: ffmpeg failed to generate .wav file.")
+
+
+# https://stackoverflow.com/a/24892274
+def sameValueRuns(arr: np.ndarray, value) -> np.ndarray:
+    # Create an array that is 1 where a is value, and pad each end with an extra 0.
+    iszero = np.concatenate(([0], np.equal(arr, value), [0]))
+    absdiff = np.abs(np.diff(iszero))
+    # Runs start and end where absdiff is 1.
+    ranges = np.where(absdiff == 1)[0].reshape(-1, 2)
+    return ranges
+
+def getClipping(channel: np.ndarray, ceiling, threshold: int) -> list:
+    clipSamples = []
+    runs = sameValueRuns(channel, ceiling)
+    for run in runs:
+        if run[1] - run[0] >= threshold:
+            clipSamples.append(run)
+    return clipSamples
+
+def channelHasClipping(channel: np.ndarray, max, min, threshold: int) -> list:
+    return getClipping(channel, max, threshold) + getClipping(channel, min, threshold)
+    
+
+def checkClipping(file: FileType, filepath: str, threshold: int = 3) -> str:
+    wav_filepath = Path(filepath)
+    newfile = False
+    if not isinstance(file, wave.WAVE):
+        newfile = True
+        wav_filepath = "{}_temp.wav".format(Path.joinpath(wav_filepath.parent, wav_filepath.stem))
+    else:
+        DEBUG('Bits per sample: {}'.format(file.info.bits_per_sample))
+        
+    if not os.path.exists(wav_filepath):
+        convertToWAV(filepath, wav_filepath)
+    
+    DEBUG('Wav file: {}'.format(wav_filepath))
+    wavFile: wave.WAVE = parseAudio(wav_filepath)
+    DEBUG('Metadata: {}'.format(wavFile.pprint()))
+
+    clips = []
+    framerate, data = wavfile.read(wav_filepath)
+
+    # Special case: 24-bit FLACs can go over sample limit and cause overflow/underflow,
+    # apply specialized algorithm to check for clicking instead.
+    # TODO: what about other bit depths?
+    if isinstance(file, flac.FLAC) and file.info.bits_per_sample == 24:
+        DEBUG("Input file is detected as 24-bit FLAC. Recommend verifing clipping in Audacity.")
+        data_deriv = np.gradient(data, axis=0)
+        maxG = np.max(data_deriv)
+        minG = np.min(data_deriv)
+        DEBUG('G: Max: {}, Min: {}'.format(maxG, minG))
+
+        # TODO: fine tune arbitrarily chosen threshold
+        # it may be possible to use 'and' since overflow/underflow will create large gradient both ways
+        if maxG > 0.8 or minG < -0.8:
+            return ":fix: Detected large gradient in 24-bit FLAC file. Please verify clipping in Audacity."
+        else:
+            return ":check: The rip is not clipping."
+
+    # +1 to min in order to mimic Audacity's Find Clipping algorithm,
+    # even though WAV samples can technically go lower
+    # TODO: 8bit not tested yet
+    limits = {
+        8:  ( 0         +1,     255         ),
+        16: (-2**15     +1,     2**15-1     ),
+        24: (-2**31     +1,     2147483392  ),
+        32: (-2**31     +1,     2**31-1     ),
+    }
+
+    # Apparently WAV 32-bit float can go over +-1.0
+    if data.dtype == np.float32:
+        data.clip(-1.0, 1.0, out=data)
+    else:
+        data.clip(limits[wavFile.info.bits_per_sample][0], limits[wavFile.info.bits_per_sample][1], out=data)
+
+    # If audio is mono, reshape data for consistency
+    if data.ndim == 1:
+        data = data[:,None]
+
+    # Find max and min values in case someone tries to fix clipping in Audacity
+    maxVals = data.max(axis=0)
+    minVals = data.min(axis=0)
+
+    DEBUG('Data type: {}'.format(data.dtype))
+    DEBUG('Max: {}'.format(maxVals))
+    DEBUG('Min: {}'.format(minVals))
+
+    debugClipSamples = []
+    upperClip = np.full(maxVals.shape, False)
+    lowerClip = np.full(minVals.shape, False)
+
+    clipSamples = []
+    for c in range(maxVals.size):
+        samples = channelHasClipping(data[:, c], maxVals[c], minVals[c], threshold)
+        for s in samples:
+            if data[s[0], c] == maxVals[c]:
+                upperClip[c] = True
+            if data[s[0], c] == minVals[c]:
+                lowerClip[c] = True
+            debugClipSamples.append((s[0] / framerate, data[s[0]:s[1], 0]))
+        clipSamples.extend(samples)
+
+    for d in debugClipSamples:
+        DEBUG(d)
+
+    clipSamples.sort(key = lambda x: (x[0], x[1])) # Sort by time for viewing purpose
+    for clipSample in clipSamples:
+        clips.append('{:.2f} sec ({} samples)'.format(clipSample[0] / framerate, clipSample[1] - clipSample[0]))
+
+    if newfile and not isinstance(file, wave.WAVE):
+        os.remove(wav_filepath)
+    
+    if len(clips) > 0:
+        msg = ""
+
+        # Detect if volume was reduced post-render
+        if newfile or data.dtype == np.float32:
+            formatMax = 1.0
+            formatMin = -1.0
+        else:
+            formatMax = limits[file.info.bits_per_sample][1]
+            formatMin = limits[file.info.bits_per_sample][0]
+
+        if np.any(np.logical_and(upperClip, maxVals < formatMax)) or np.any(np.logical_and(lowerClip, minVals > formatMin)):
+            msg = " Post-render volume reduction detected, please lower the volume before rendering."
+        
+        if len(clips) > 10:
+            msg = ":fix: The rip is heavily clipping." + msg
+        else:
+            msg = ":fix: The rip is clipping at: " + ", ".join(clips) + "." + msg
+        
+        return msg
+    else:
+        return ":check: The rip is not clipping."
+
+
+#=======================================#
+#            Main Function              #
+#=======================================#
+
+def simpleQoC(url: str) -> str:
+    if not os.path.exists(DOWNLOAD_DIR):
+        os.mkdir(DOWNLOAD_DIR)
+    
+    filepath = None
+    try:
+        filepath = downloadAudioFromUrl(url)
+        print("Downloaded audio: " + Path(filepath).name)
+    
+    except QoCException as e:
+        print(e.message)
+    
+    else:
+        file = parseAudio(filepath)
+        print("File metadata: " + file.pprint())
+
+        try:
+            print(checkBitrate(file))
+        except QoCException as e:
+            print(e.message)
+
+        try:
+            print(checkClipping(file, filepath))
+        except QoCException as e:
+            print(e.message)
+    
+    finally:
+        if filepath:
+            os.remove(filepath)
+
+    # TODO
+    return ""
+
+
+#=======================================#
+#           Script Testing              #
+#=======================================#
+import sys
+
+if __name__ == '__main__':
+    if '-d' in sys.argv:
+        print('DEBUG MODE ENABLED')
+        DEBUG_MODE = True
+    
+    url = input('Paste the path of the audio you want to check: ')
+    simpleQoC(url)
+    
---
+++ test.py
@@ -0,0 +1,250 @@
+import unittest
+import os
+from pathlib import Path
+from inspect import getsourcefile
+from mutagen import File
+
+from simpleQoC import downloadAudioFromUrl, checkBitrate, checkClipping, QoCException
+
+TEST_DIR = Path(os.path.abspath(getsourcefile(lambda:0))).parent / 'tests'
+
+class TestDownload(unittest.TestCase):
+    """
+    Test suites for the downloadAudioFromUrl function
+    """
+    DOWNLOAD_DIR = Path(os.path.abspath(getsourcefile(lambda:0))).parent / 'audioDownloads'
+
+    def __init__(self, *args, **kwargs):
+        super(TestDownload, self).__init__(*args, **kwargs)
+        if not os.path.exists(self.DOWNLOAD_DIR):
+            os.mkdir(self.DOWNLOAD_DIR)
+
+    # Successful downloads
+    def testSuccessSiivaGunner(self):
+        filepath = downloadAudioFromUrl("https://siiva-gunner.com/?id=vnrufKKnxu")
+        self.assertEqual(filepath, self.DOWNLOAD_DIR / 'goodQuality.mp3')
+        if filepath:
+            os.remove(filepath)
+
+    def testSuccessSiivaGunnerV2(self):
+        filepath = downloadAudioFromUrl("https://185.142.239.147/?id=vnrufKKnxu")
+        self.assertEqual(filepath, self.DOWNLOAD_DIR / 'goodQuality.mp3')
+        if filepath:
+            os.remove(filepath)
+
+    def testSuccessDrive(self):
+        filepath = downloadAudioFromUrl("https://drive.google.com/file/d/1ofQMUh1xtItM3a1VXATRovYL9nYVYets/view?usp=sharing")
+        self.assertEqual(filepath, self.DOWNLOAD_DIR / 'goodQuality.mp3')
+        if filepath:
+            os.remove(filepath)
+
+    def testSuccessDriveV2(self):
+        filepath = downloadAudioFromUrl("https://drive.google.com/file/d/1ofQMUh1xtItM3a1VXATRovYL9nYVYets")
+        self.assertEqual(filepath, self.DOWNLOAD_DIR / 'goodQuality.mp3')
+        if filepath:
+            os.remove(filepath)
+
+    def testSuccessDriveV3(self):
+        filepath = downloadAudioFromUrl("https://drive.google.com/open?id=1ofQMUh1xtItM3a1VXATRovYL9nYVYets")
+        self.assertEqual(filepath, self.DOWNLOAD_DIR / 'goodQuality.mp3')
+        if filepath:
+            os.remove(filepath)
+
+    def testSuccessDriveV4(self):
+        filepath = downloadAudioFromUrl("https://drive.google.com/uc?id=1ofQMUh1xtItM3a1VXATRovYL9nYVYets&export=download")
+        self.assertEqual(filepath, self.DOWNLOAD_DIR / 'goodQuality.mp3')
+        if filepath:
+            os.remove(filepath)
+
+    def testSuccessDropbox(self):
+        filepath = downloadAudioFromUrl("https://www.dropbox.com/scl/fi/stkgm8qtw6m9oq5dbeg76/goodQuality.mp3?rlkey=wxhi0wu55a4d4tsppe6buu5by&st=vi8io7zw&dl=0")
+        self.assertEqual(filepath, self.DOWNLOAD_DIR / 'goodQuality.mp3')
+        if filepath:
+            os.remove(filepath)
+
+    # TODO: Ask livvy for neocities sample
+    # TODO: b.catgirlsare.sexy does not work for some reason
+    # TODO: Try a large Google Drive link (should work)
+
+    # Failed downloads
+    def testFailureSiivaGunner(self):
+        with self.assertRaises(QoCException):
+            filepath = downloadAudioFromUrl("https://siiva-gunner.com/?id=vnrufKKnx")
+            if filepath:
+                os.remove(filepath)
+
+    def testFailureDrive(self):
+        with self.assertRaises(QoCException):
+            filepath = downloadAudioFromUrl("https://drive.google.com/file/d/1ofQMUh1xtItM3a1VXATRovYL9nYVYet/view?usp=sharing")
+            if filepath:
+                os.remove(filepath)
+
+
+class TestBitrate(unittest.TestCase):
+    """
+    Test suites for the checkBitrate function
+    """
+    # Helper strings and functions
+    BITRATE_OK_MSG = ":check: Bitrate is OK."
+    BITRATE_LOSSLESS_MSG = ":check: Lossless file is OK."
+
+    def BITRATE_LOW_MSG(self, filetype, kbps): 
+        return ":fix: The {} file's bitrate is {}kbps. Please re-render at 320kbps.".format(filetype, kbps)
+
+    # Lossless
+    def testBitrateFLAC(self):
+        msg = checkBitrate(File(TEST_DIR / 'goodQuality.flac'))
+        self.assertEqual(msg, self.BITRATE_LOSSLESS_MSG)
+
+    def testBitrateWAV(self):
+        msg = checkBitrate(File(TEST_DIR / 'goodQuality.wav'))
+        self.assertEqual(msg, self.BITRATE_LOSSLESS_MSG)
+
+    # Bitrate OK
+    def testBitrateAIFF(self):
+        msg = checkBitrate(File(TEST_DIR / 'goodQuality.aiff'))
+        self.assertEqual(msg, self.BITRATE_OK_MSG)
+
+    def testBitrateMP2(self):
+        msg = checkBitrate(File(TEST_DIR / 'goodQuality.mp2'))
+        self.assertEqual(msg, self.BITRATE_OK_MSG)
+
+    def testBitrateMP3(self):
+        msg = checkBitrate(File(TEST_DIR / 'goodQuality.mp3'))
+        self.assertEqual(msg, self.BITRATE_OK_MSG)
+
+    def testLowBitrateMP4(self):
+        msg = checkBitrate(File(TEST_DIR / 'goodQuality.mp4'))
+        self.assertEqual(msg, self.BITRATE_OK_MSG)
+
+    def testBitrateOGG(self):
+        msg = checkBitrate(File(TEST_DIR / 'goodQuality.ogg'))
+        self.assertEqual(msg, self.BITRATE_OK_MSG)
+
+    # Low bitrate
+    def testLowBitrateMP3(self):
+        msg = checkBitrate(File(TEST_DIR / 'lowBitrate.mp3'))
+        self.assertEqual(msg, self.BITRATE_LOW_MSG('MP3', 128))
+    
+    def testLowBitrateMP4(self):
+        msg = checkBitrate(File(TEST_DIR / 'clipping6lowBitrate.mp4'))
+        self.assertEqual(msg, self.BITRATE_LOW_MSG('MP4', 192))
+    
+    def testLowBitrateM4A(self):
+        msg = checkBitrate(File(TEST_DIR / 'lowBitrate.m4a'))
+        self.assertEqual(msg, self.BITRATE_LOW_MSG('MP4', 127))
+
+    def testLowBitrateOGG(self):
+        msg = checkBitrate(File(TEST_DIR / 'lowBitrate.ogg'))
+        self.assertEqual(msg, self.BITRATE_LOW_MSG('OggVorbis', 192))
+    
+
+class TestClipping(unittest.TestCase):
+    """
+    Test suites for the checkClipping function
+    """
+    # Helper strings and functions
+    CLIPPING_OK_MSG = ":check: The rip is not clipping."
+    CLIPPING_HEAVY_MSG = ":fix: The rip is heavily clipping."
+    CLIPPING_REDUCED_MSG = " Post-render volume reduction detected, please lower the volume before rendering."
+
+    def CLIPPING_MSG(self, clips): 
+        return ":fix: The rip is clipping at: " + ", ".join(clips) + "."
+    
+    def checkFilepathClipping(self, filepath):
+        return checkClipping(File(filepath), filepath)
+    
+    def clip(self, t, n):
+        return '{:.2f} sec ({} samples)'.format(t, n)
+
+    # No clipping
+    def testNoClippingMP3(self):
+        msg = self.checkFilepathClipping(TEST_DIR / 'goodQuality.mp3')
+        self.assertEqual(msg, self.CLIPPING_OK_MSG)
+    
+    def testNoClippingFLAC(self):
+        msg = self.checkFilepathClipping(TEST_DIR / 'goodQuality.flac')
+        self.assertEqual(msg, self.CLIPPING_OK_MSG)
+    
+    def testNoClippingWAV(self):
+        msg = self.checkFilepathClipping(TEST_DIR / 'goodQuality.wav')
+        self.assertEqual(msg, self.CLIPPING_OK_MSG)
+
+    def testNoClippingWAVMono(self):
+        msg = self.checkFilepathClipping(TEST_DIR / 'goodQualityMono.wav')
+        self.assertEqual(msg, self.CLIPPING_OK_MSG)
+
+    def testAlmostClippingMP3(self):
+        msg = self.checkFilepathClipping(TEST_DIR / 'almostClipping.mp3')
+        self.assertEqual(msg, self.CLIPPING_OK_MSG)
+
+    # Clipping
+    def testClipping1(self):
+        msg = self.checkFilepathClipping(TEST_DIR / 'clipping1.mp3')
+        self.assertEqual(msg, self.CLIPPING_HEAVY_MSG)
+    
+    def testClipping2(self):
+        msg = self.checkFilepathClipping(TEST_DIR / 'clipping2.mp3')
+        self.assertEqual(msg, self.CLIPPING_MSG([
+            self.clip(12.13, 4), 
+            self.clip(13.01, 3),
+        ]))
+    
+    def testClipping2inverted(self):
+        msg = self.checkFilepathClipping(TEST_DIR / 'clipping2inverted.wav')
+        self.assertEqual(msg, self.CLIPPING_MSG([
+            self.clip(12.13, 3),
+        ]))
+    
+    def testClipping3(self):
+        msg = self.checkFilepathClipping(TEST_DIR / 'clipping3.wav')
+        self.assertEqual(msg, self.CLIPPING_HEAVY_MSG + self.CLIPPING_REDUCED_MSG)
+
+    def testClipping4(self):
+        msg = self.checkFilepathClipping(TEST_DIR / 'clipping4.wav')
+        self.assertEqual(msg, self.CLIPPING_HEAVY_MSG)
+
+    def testClipping5(self):
+        msg = self.checkFilepathClipping(TEST_DIR / 'clipping5.ogg')
+        self.assertEqual(msg, self.CLIPPING_MSG([
+            self.clip(6.23, 4),
+            self.clip(7.18, 5),
+            self.clip(7.97, 4), 
+            self.clip(9.82, 3),
+            self.clip(12.04, 5), 
+            self.clip(13.01, 3),
+        ]))
+
+    def testClipping6(self):
+        msg = self.checkFilepathClipping(TEST_DIR / 'clipping6lowBitrate.mp4')
+        self.assertEqual(msg, self.CLIPPING_HEAVY_MSG)
+
+    def testClipping24bitFLAC(self):
+        msg = self.checkFilepathClipping(TEST_DIR / 'clipping24bit.flac')
+        self.assertEqual(msg, ":fix: Detected large gradient in 24-bit FLAC file. Please verify clipping in Audacity.")
+
+    def testClipping24bitWAV(self):
+        msg = self.checkFilepathClipping(TEST_DIR / 'clipping24bit.wav')
+        self.assertEqual(msg, self.CLIPPING_HEAVY_MSG)
+
+    def testClipping32bitWAV(self):
+        msg = self.checkFilepathClipping(TEST_DIR / 'clipping32bitfloat.wav')
+        self.assertEqual(msg, self.CLIPPING_MSG([
+            self.clip(12.99, 6),
+            self.clip(12.99, 7),
+            self.clip(13.00, 6), 
+            self.clip(13.01, 8),
+            self.clip(13.01, 4),
+            self.clip(13.01, 4), 
+            self.clip(13.02, 10),
+        ]))
+
+import simpleQoC
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        print('DEBUG MODE ENABLED')
+        simpleQoC.DEBUG_MODE = True
+    
+    unittest.main()
