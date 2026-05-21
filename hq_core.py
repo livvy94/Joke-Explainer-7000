fe42ab4587b4e0ed5657c30dc1c700514690243f
Ahmaykmewsik <ahmaykmewsik@gmail.com>
2026-05-21 01:02:04 -0700

    only repost vet message on pin (not on !vet commands)

--- hq_core.py
+++ hq_core.py
@@ -1169,7 +1169,7 @@ async def vet_rip_or_url(rip_text_or_url: str, desc: VetRipDesc, guild: discord.
     if past_vet_message:
         errors = await discord_edit_message(past_vet_message, vet_report_text)
         error_strings.extend(errors)
-    elif desc.message and channel_is_types(desc.message.channel, ['QOC']):
+    elif desc.is_new_pinned_message and desc.message and channel_is_types(desc.message.channel, ['QOC']):
         await send(vet_report_text, desc.message.channel)
 
     if not everything_passed and desc.is_new_pinned_message and len(issue_list):
